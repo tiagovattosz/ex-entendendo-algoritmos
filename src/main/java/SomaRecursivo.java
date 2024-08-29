a06f16da086087a8db4b81f224835487d9016232
tiagovattos <tiagovattosz@gmail.com>
2024-08-29 00:29:22 -0300

    Correcao soma recursivo

--- src/main/java/SomaRecursivo.java
+++ src/main/java/SomaRecursivo.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SomaRecursivo {
 
     public static void main(String[] args) {
-        List<Integer> array = Arrays.asList(1, 2, 3, 4, 90);
+        List<Integer> array = new ArrayList<>(Arrays.asList(1,2,3,4));
         Integer soma = soma(array);
         System.out.println(soma);
     }
